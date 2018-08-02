a7d16c063d71aa57b1f947f6f250ba66117c23b1
avemoi <cmageiridis@gmail.com>
2018-08-02 21:27:20 +0300

    Spaces fixes

--- Despeckle_pyqt/py_src/gui_main.py
+++ Despeckle_pyqt/py_src/gui_main.py
@@ -35,10 +35,6 @@ except AttributeError:
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig)
 
-
-
-
-
 class Gui_Main(QtGui.QMainWindow):
     """
     The main window of the app
@@ -54,14 +50,10 @@ class Gui_Main(QtGui.QMainWindow):
         to slots
         """
         super(Gui_Main, self).__init__()
-        self.setupUi(main)
-        
-        
+        self.setupUi(main)   
         self.execute = Execute()
-        
         self.connect(self.execute, SIGNAL("setOutput(QString)"),self.setOutput)
         self.connect(self.execute, SIGNAL("finished(bool)"),self.finished)
-        
         self.step = 0
 
     def setupUi(self, MainWindow):
@@ -88,31 +80,23 @@ class Gui_Main(QtGui.QMainWindow):
         self.widget_2.setGeometry(QtCore.QRect(410, 750, 291, 41))
         self.widget_2.setObjectName(_fromUtf8("widget_2"))
         
-        
         self.widget_3 = QtGui.QWidget(self.centralwidget)
         self.widget_3.setGeometry(QtCore.QRect(10, 40, 691, 331))
         self.widget_3.setObjectName(_fromUtf8("widget_3"))
 
-
         MainWindow.setCentralWidget(self.centralwidget)
-
-########################################################################################################################3
 
         self.textEdit = QtGui.QTextEdit(self.widget_3)
         self.textEdit.setGeometry(QtCore.QRect(110, 50, 501, 31))
         self.textEdit.setObjectName(_fromUtf8("textEdit"))
 
-
-
         self.textEdit_2 = QtGui.QTextEdit(self.widget_3)
         self.textEdit_2.setGeometry(QtCore.QRect(110, 100, 501, 31))
         self.textEdit_2.setObjectName(_fromUtf8("textEdit_2"))
 
-
         self.textEdit_3 = QtGui.QTextEdit(self.widget_3)
         self.textEdit_3.setGeometry(QtCore.QRect(110, 150, 501, 31))
         self.textEdit_3.setObjectName(_fromUtf8("textEdit_3"))
-
 
         self.textEdit_4 = QtGui.QTextEdit(self.widget_3)
         self.textEdit_4.setGeometry(QtCore.QRect(110, 200, 251, 31))
@@ -122,15 +106,10 @@ class Gui_Main(QtGui.QMainWindow):
         self.textEdit_5.setGeometry(QtCore.QRect(110, 250, 251, 31))
         self.textEdit_5.setObjectName(_fromUtf8("textEdit_5"))
         
-        
         self.plainTextEdit = QtGui.QPlainTextEdit(self.centralwidget)
         self.plainTextEdit.setGeometry(QtCore.QRect(119, 420, 501, 321))
         self.plainTextEdit.setObjectName(_fromUtf8("plainTextEdit"))
         self.plainTextEdit.setReadOnly(True)
-
-        
-        
-########################################################################################################################3
   
         self.label = QtGui.QLabel(self.widget_2)
         self.label.setGeometry(QtCore.QRect(20, 10, 201, 21))
@@ -140,21 +119,17 @@ class Gui_Main(QtGui.QMainWindow):
         self.label_2.setGeometry(QtCore.QRect(30, 50, 41, 31))
         self.label_2.setObjectName(_fromUtf8("label_2"))
 
-
         self.label_3 = QtGui.QLabel(self.widget_3)
         self.label_3.setGeometry(QtCore.QRect(30, 100, 21, 31))
         self.label_3.setObjectName(_fromUtf8("label_3"))
-
 
         self.label_4 = QtGui.QLabel(self.widget_3)
         self.label_4.setGeometry(QtCore.QRect(30, 150, 51, 31))
         self.label_4.setObjectName(_fromUtf8("label_4"))
 
-
         self.label_5 = QtGui.QLabel(self.widget_3)
         self.label_5.setGeometry(QtCore.QRect(30, 200, 81, 31))
         self.label_5.setObjectName(_fromUtf8("label_5"))
-
 
         self.label_6 = QtGui.QLabel(self.widget_3)
         self.label_6.setGeometry(QtCore.QRect(30, 250, 51, 31))
@@ -164,7 +139,6 @@ class Gui_Main(QtGui.QMainWindow):
         self.label_7.setGeometry(QtCore.QRect(310, 380, 101, 31))
         self.label_7.setObjectName(_fromUtf8("label_7"))
 
-
         self.label_8 = QtGui.QLabel(self.widget_3)
         self.label_8.setGeometry(QtCore.QRect(450, 200, 151, 20))
         self.label_8.setObjectName(_fromUtf8("label_8"))
@@ -172,14 +146,6 @@ class Gui_Main(QtGui.QMainWindow):
         self.label_9 = QtGui.QLabel(self.widget_3)
         self.label_9.setGeometry(QtCore.QRect(450, 250, 151, 20))
         self.label_9.setObjectName(_fromUtf8("label_9"))
-########################################################################################################################3
-
-
-        
-        
-        
-
-
 
         icon = QtGui.QIcon()
         icon.addPixmap(QtGui.QPixmap(_fromUtf8("/opt/Despeckle_pyqt/icons/view_tree.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
@@ -192,7 +158,6 @@ class Gui_Main(QtGui.QMainWindow):
         self.pushButton.setObjectName(_fromUtf8("pushButton"))
         self.pushButton.clicked.connect(self.inputButtonClicked)
         
-
         self.pushButton_2 = QtGui.QPushButton(self.widget_3)
         self.pushButton_2.setGeometry(QtCore.QRect(630, 100, 41, 31))
         self.pushButton_2.setText(_fromUtf8(""))
@@ -201,7 +166,6 @@ class Gui_Main(QtGui.QMainWindow):
         self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
         self.pushButton_2.clicked.connect(self.outputButtonClicked)
 
-
         self.pushButton_3 = QtGui.QPushButton(self.widget_3)
         self.pushButton_3.setGeometry(QtCore.QRect(630, 150, 41, 31))
         self.pushButton_3.setText(_fromUtf8(""))
@@ -209,11 +173,6 @@ class Gui_Main(QtGui.QMainWindow):
         self.pushButton_3.setIconSize(QtCore.QSize(42, 42))
         self.pushButton_3.setObjectName(_fromUtf8("pushButton_3"))
         self.pushButton_3.clicked.connect(self.inputKernelButtonClicked)
-
-
-
-
-      
         
         self.pushButton_4 = QtGui.QPushButton(self.widget_3)
         self.pushButton_4.setGeometry(QtCore.QRect(630, 200, 41, 31))
@@ -221,7 +180,6 @@ class Gui_Main(QtGui.QMainWindow):
         self.pushButton_4.setIcon(icon)
         self.pushButton_4.setIconSize(QtCore.QSize(42, 42))
         self.pushButton_4.clicked.connect(self.statsButton)
-
         
         self.pushButton_5 = QtGui.QPushButton(self.widget_3)
         self.pushButton_5.setGeometry(QtCore.QRect(630, 250, 41, 31))
@@ -229,20 +187,6 @@ class Gui_Main(QtGui.QMainWindow):
         self.pushButton_5.setIcon(icon)
         self.pushButton_5.setIconSize(QtCore.QSize(42, 42))
         self.pushButton_5.clicked.connect(self.statsButton2)
-        
-    
-        
-        
-        
-        
-
-        
-########################################################################################################################3
-        
-        
-        
-        
-        
         
         self.menubar = QtGui.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 716, 20))
@@ -256,9 +200,6 @@ class Gui_Main(QtGui.QMainWindow):
 
         MainWindow.setMenuBar(self.menubar)
 
-        
-        
-        
         self.toolBar = QtGui.QToolBar(MainWindow)
         self.toolBar.setObjectName(_fromUtf8("toolBar"))
         MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.toolBar)
@@ -269,9 +210,6 @@ class Gui_Main(QtGui.QMainWindow):
         self.action_Run.setIcon(icon1)
         self.action_Run.setObjectName(_fromUtf8("action_Run"))
         self.action_Run.triggered.connect(self.action_Run_Clicked)
-        
-
-
 
         self.actionStop = QtGui.QAction(MainWindow)
         icon2 = QtGui.QIcon()
@@ -280,13 +218,10 @@ class Gui_Main(QtGui.QMainWindow):
         self.actionStop.setObjectName(_fromUtf8("actionStop"))
         self.actionStop.triggered.connect(self.action_Stop_Clicked)
 
-
         self.action_Exit = QtGui.QAction(MainWindow)
-        
-        
+                
         icon3 = QtGui.QIcon()
         icon3.addPixmap(QtGui.QPixmap(_fromUtf8("/opt/Despeckle_pyqt/icons/fileclose.png")), QtGui.QIcon.Normal, QtGui.QIcon.On)
-        
         
         self.action_Exit.setIcon(icon3)
         self.action_Exit.setObjectName(_fromUtf8("action_Exit"))
@@ -297,8 +232,6 @@ class Gui_Main(QtGui.QMainWindow):
         self.toolBar.addAction(self.action_Exit)
         self.toolBar.addSeparator()
         
-        
-
         self.action_About_Despeckle = QtGui.QAction(MainWindow)
         self.action_About_Despeckle.setObjectName(_fromUtf8("action_About_Despeckle"))
         
@@ -310,15 +243,11 @@ class Gui_Main(QtGui.QMainWindow):
         
         self.menubar.addAction(self.menu_File.menuAction())
         self.menubar.addAction(self.menu_Help.menuAction())
-        
-       
 
         self.retranslateUi(MainWindow)
 
-
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
-        
-        
+                
     def retranslateUi(self, MainWindow):
         
         MainWindow.setWindowTitle(_translate("Despeckle", "Despeckle", None))
@@ -343,8 +272,6 @@ class Gui_Main(QtGui.QMainWindow):
         self.action_Exit.setText(_translate("MainWindow", "&Exit", None))
         self.action_About_Despeckle.setText(_translate("MainWindow", "&About Despeckle", None))
 
-
-
     def inputButtonClicked(self):
         """
         When button pushed, select input .cub file
@@ -355,9 +282,6 @@ class Gui_Main(QtGui.QMainWindow):
         self.textEdit.setText(filename)
         self.textEdit.setEnabled(0)
         
-        
-        
-
     def outputButtonClicked(self):
         """
         When button pushed, select output .cub file
@@ -365,12 +289,9 @@ class Gui_Main(QtGui.QMainWindow):
         user from changing path manualy
         """
         filename = QtGui.QFileDialog.getSaveFileName(None,"Select file",".cub",".cub")
-        #filename=QtGui.QFileDialog.getExistingDirectory(parent=None, caption='', directory='')
         
         self.textEdit_2.setText(filename)
         self.textEdit_2.setEnabled(0)
-
-
 
     def inputKernelButtonClicked(self):
         """
@@ -381,8 +302,6 @@ class Gui_Main(QtGui.QMainWindow):
         filename=QtGui.QFileDialog.getOpenFileName(None, 'Select file',"*.txt","*.txt")
         self.textEdit_3.setText(filename)
         self.textEdit_3.setEnabled(0)
-
-
 
     def action_Run_Clicked(self):
         """
@@ -401,24 +320,11 @@ class Gui_Main(QtGui.QMainWindow):
         l_value           = self.textEdit_5.toPlainText()
         iterations        = self.textEdit_4.toPlainText()
         
-        
-        #input_cub_path    = "/home/enos/workspace/Despeckle_filter/source/data/T19_crop.cub"
-        #output_cub_path   = "/home/enos/Desktop"
-        #input_kernel_path = "/home/enos/workspace/Despeckle_filter/source/kernel/mykern.txt"
-        #l_value           = "0.08"
-        #iterations        = "5"
-        
-        
-
-        
-        
-        
         if(not l_value):
             l_value='-l 0.08'
         else:
             l_value='-l '+ l_value
-            
-            
+                        
         if(not iterations):
             iterations='-i 100'
             self.maxIterations = 100
@@ -429,27 +335,17 @@ class Gui_Main(QtGui.QMainWindow):
         # Set max number to progress bar --> number of total iterations, 100 by default    
         self.progressBar.setMaximum(self.maxIterations)
         
-        
         if( not input_cub_path or not output_cub_path or not input_kernel_path):
             print("Error")
         else:
             self.cmd="sh /opt/Despeckle_pyqt/Despeckle/a_despeckle.sh "+ input_cub_path+' '+ input_kernel_path+' '+ ' '+output_cub_path +' '+l_value+' ' +iterations
             
-            
-            
-            
-        
             self.execute.setCmd(self.cmd)
             
             self.execute.start()
             
             print("is finished",self.execute.isFinished())
             
-            
-            
-            
-    
-    
     def finished(self,completed):
         if (self.execute.isRunning()):
             return
@@ -460,8 +356,6 @@ class Gui_Main(QtGui.QMainWindow):
             print("stopped")
             self.label.setText("Cancelled")
      
-     
-     
     def setOutput(self,mystr):
         """
         Redirects stdout given from signal
@@ -469,11 +363,8 @@ class Gui_Main(QtGui.QMainWindow):
         string to bar and label
         """
         self.plainTextEdit.appendPlainText(mystr)
-        
         self.setOutputBar(mystr)
         self.setOutputLabel(mystr)       
-    
-    
     
     def setOutputBar(self,mystr):
         """
@@ -488,12 +379,7 @@ class Gui_Main(QtGui.QMainWindow):
         """
         if(mystr.startswith("Iteration")):
             currentIteration = int( mystr[9:] )
-            self.progressBar.setValue(currentIteration)
-            
-        
-        
-            
-            
+            self.progressBar.setValue(currentIteration)       
     
     def setOutputLabel(self,mystr):
         """
@@ -508,40 +394,25 @@ class Gui_Main(QtGui.QMainWindow):
         if(mystr.startswith("Iteration")):
             self.label.setText(mystr)        
 
-
-
     def action_Stop_Clicked(self):
         """
         A not-an-elegant
         way to stop the thread 
         """
-        print("exiiit")
-        #if self.execute.isRunning():
         self.execute.stop()
-        #self.execute.wait()
-        
-        
-        
-        
-     
+             
     def statsButton(self):
         """
         When button pushed, a stats
         frame pops up with the stats
         of the input image.
         """
-      
         path = self.textEdit.toPlainText()
         if (not path):
             pass
         else:
             self.stats1 = StatsPopup(path)
-            self.stats1.start()
-        
-        
-        
-        
-        
+            self.stats1.start()   
         
     def statsButton2(self):
         """
@@ -555,22 +426,18 @@ class Gui_Main(QtGui.QMainWindow):
         else:
             self.stats2 = StatsPopup(path)
             self.stats2.start()
-        
-        
-        
+         
     def aboutClicked(self):
         """
         When button pushed, pops up
         a frame with a text read from
         a file
         """
-        text = []
-        
+        text = []  
         file = open("/opt/Despeckle_pyqt/about.txt", 'r')
         for line in file:
             text.append(line)
-            
-        #print("\n".join(text))    
+
         self.about = MyPopup("about")
         self.about.setText("".join(text))
         
