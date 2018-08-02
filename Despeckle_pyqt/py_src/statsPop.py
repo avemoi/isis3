8916ca759979185233885fe289745c2604de474b
avemoi <cmageiridis@gmail.com>
2018-08-02 21:29:17 +0300

    Spaces fixes

--- Despeckle_pyqt/py_src/statsPop.py
+++ Despeckle_pyqt/py_src/statsPop.py
@@ -3,13 +3,10 @@ Created on Sep 10, 2015
 
 @author: enos
 '''
-
-
 from PyQt4 import QtCore, QtGui
 import subprocess
 import sys
 from popup import MyPopup
-
 
 class StatsPopup(MyPopup):
     """
@@ -18,22 +15,13 @@ class StatsPopup(MyPopup):
     to the new frame
     """
     def __init__(self,path):
-        
         MyPopup.__init__(self, path)
-        
-        
         self.cmd="stats FROM="+path
-        
-        
-        
-        
+           
     def start(self):
         self.myRun()
         
-    
-        
     def myRun(self):
-        
         self.p = subprocess.Popen(self.cmd,shell=True,stdout=subprocess.PIPE)
         for line in self.p.stdout:
             line = line.rstrip()
