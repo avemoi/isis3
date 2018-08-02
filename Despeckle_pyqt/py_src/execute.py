b5a1a796055957a719b461c8c85c4ae40eef93eb
avemoi <cmageiridis@gmail.com>
2018-08-02 21:20:50 +0300

    Spaces fixes

--- Despeckle_pyqt/py_src/execute.py
+++ Despeckle_pyqt/py_src/execute.py
@@ -15,7 +15,6 @@ except ImportError:
         # we are using Python3 so QString is not defined  
     QString = str
 
-
 class Execute(QThread):
     """
     Create new thread to execute the script
@@ -27,47 +26,30 @@ class Execute(QThread):
     """
     
     def __init__(self):
-        
         QThread.__init__(self)
- 
         print("execute constructor")
-        
         self.output=[]
              
     def run(self):
         print("enter run")
         self.runScript()
-        
         self.emit(SIGNAL("finished(bool)"),True)
         print("finished from run")
 
-     
     def stop(self):
-       
         self.terminate()
         self.p.kill()
         self.emit(SIGNAL("finished(bool)"),False)
         
-
     def setCmd(self,cmd):
-        
         self.cmd = cmd   
         print(cmd)
     
-    
-        
-           
-        
-    def runScript(self):
-        
-       
-        
+    def runScript(self):    
         self.p = subprocess.Popen(self.cmd,shell=True,stdout=subprocess.PIPE)
-        
         for line in self.p.stdout:
             line = line.rstrip()
             line=line.decode(sys.stdout.encoding)
-           
             self.emit(SIGNAL("setOutput(QString)"), line)
             
            
